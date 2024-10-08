63fd4dc38a3edfe0b89874c99ce379612a443b1e
Juliana Gagliano <julianagagliano1@gmail.com>
2024-10-08 18:45:58 -0300

    feat: tela inicial

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+// src/App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/pages/inicial';
 
+const Stack = createStackNavigator();
 
-export default class app extends React.Component{
-  render(){
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ headerShown: false }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-    return(
-      <ScrollView style={{marginTop:55}}>
-      
-      </ScrollView >
-    )
-  }
-}
+export default App;
